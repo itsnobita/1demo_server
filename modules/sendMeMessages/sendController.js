3f4978ed747b225d0422a691a09c09cd0aa90d51
NAVEEN GOYAL -X (navgoyal - HCL TECHNOLOGIES LIMITED at Cisco) <navgoyal@cisco.com>
2024-07-05 19:39:37 +0530

    fic

--- modules/sendMeMessages/sendController.js
+++ modules/sendMeMessages/sendController.js
@@ -7,30 +7,31 @@ router.post("/nobi", async (req, res) => {
   try {
     console.log("in sendMeMessage /nobi/");
     console.log(req.headers.cookie);
-      let cookies = parseCookieString(req.headers.cookie);
+    let cookies = parseCookieString(req.headers.cookie)||"";
     // console.log(req.body)
-      const result = await saveMessage(req.body, req.headers);
-      if (cookies.postal == result.ipObj.postal) {
-          result.result.p=true
+    const result = await saveMessage(req.body, req.headers);
+    if (cookies.postal == result.ipObj.postal) {
+        result.result.p=true
       }
       if (cookies.domain == result.ipObj.domain) {
-          result.result.d=true
+        result.result.d=true
       }
       if (cookies.city == result.ipObj.city) {
-          result.result.c=true
+        result.result.c=true
       }
       if (cookies.route == result.ipObj.route) {
-          result.result.r=true
+        result.result.r=true
       }
       if (cookies.ip == result.ipObj.ip) {
-          result.result.i=true
-      }
-      for (let key in result.ipObj) {
-          res.cookie(key, result.ipObj[key])
-      }
+        result.result.i=true
+    }
+    for (let key in result.ipObj) {
+      res.cookie(key, result.ipObj[key]);
+    }
     delete result.ipObj;
     res.status(result.statusCode).send(result);
   } catch (error) {
+      console.log(error)
     res.status(error.statusCode).send(error);
   }
 });
