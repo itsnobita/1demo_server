a18e6adb31f197630c070c9445e8f5a798101bd1
NAVEEN GOYAL -X (navgoyal - HCL TECHNOLOGIES LIMITED at Cisco) <navgoyal@cisco.com>
2024-07-05 19:24:05 +0530

    fic

--- modules/sendMeMessages/dao/index.js
+++ modules/sendMeMessages/dao/index.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const messagesDao = {
     saveMessage: async (obj) => {
         try {
-            let db =""
-                // await mongoose.model("messages").create(obj);
+            let db = await mongoose.model("messages").create(obj);
             return {
                 status: "success",
                 statusCode: 200,
