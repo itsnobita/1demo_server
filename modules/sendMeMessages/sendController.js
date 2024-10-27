9f8a4fd5fa9cd2634eb0f1b090eb42dd367894a8
Naveen Goyal <navgoyal@cisco.com>
2024-10-27 20:50:00 +0530

    addmemsg fix

--- modules/sendMeMessages/sendController.js
+++ modules/sendMeMessages/sendController.js
@@ -6,29 +6,28 @@ const router = express.Router();
 router.post("/nobi", async (req, res) => {
   try {
     console.log("in sendMeMessage /nobi/");
-    console.log(req.headers.cookie);
     let cookies = parseCookieString(req.headers.cookie||"");
     console.log(cookies,"--cookies")
     const result = await saveMessage(req.body, req.headers);
-    if (cookies.postal == result.ipObj.postal) {
-        result.result.p=true
-      }
-      if (cookies.domain == result.ipObj.domain) {
-        result.result.d=true
-      }
-      if (cookies.city == result.ipObj.city) {
-        result.result.c=true
-      }
-      if (cookies.route == result.ipObj.route) {
-        result.result.r=true
-      }
-      if (cookies.ip == result.ipObj.ip) {
-        result.result.i=true
-    }
-    for (let key in result.ipObj) {
-      res.cookie(key, result.ipObj[key]);
-    }
-    delete result.ipObj;
+    // if (cookies.postal == result.ipObj.postal) {
+    //     result.result.p=true
+    //   }
+    //   if (cookies.domain == result.ipObj.domain) {
+    //     result.result.d=true
+    //   }
+    //   if (cookies.city == result.ipObj.city) {
+    //     result.result.c=true
+    //   }
+    //   if (cookies.route == result.ipObj.route) {
+    //     result.result.r=true
+    //   }
+    //   if (cookies.ip == result.ipObj.ip) {
+    //     result.result.i=true
+    // }
+    // for (let key in result.ipObj) {
+    //   res.cookie(key, result.ipObj[key]);
+    // }
+    // delete result.ipObj;
     res.status(result.statusCode).send(result);
   } catch (error) {
       console.log(error)
--- modules/sendMeMessages/sendService.js
+++ modules/sendMeMessages/sendService.js
@@ -6,27 +6,20 @@ export const saveMessage = async (obj, headers) => {
         ...obj,
         deviceDetails: { ...obj.deviceDetails, ...headers,  },
       }
+      console.log(`Data of message sending me message ${finalData}`)
       let ipData = await ipConfigAdapter.getData(finalData.deviceDetails["cf-connecting-ip"]);
       let ipObj ={}
       if (ipData.status == "success") {
-           ipObj = {
-              domain: ipData.result.asn.domain,
-              route: ipData.result.asn.route,
-              ip: ipData.result.ip,
-              country: ipData.result.country_name,
-              city: ipData.result.city,
-              region: ipData.result.region,
-              latitude: ipData.result.latitude,
-              longitude: ipData.result.longitude,
-              postal: ipData.result.postal,
-          };
-          finalData={...finalData,...ipData.result}
+        if (ipData.result) {
+          ipObj = ipData.result
+        }
+          finalData={...finalData,...ipObj}
       } else {
           console.log("Error in ipdata fetching")
       }
     let result = await messagesDao.saveMessage(finalData);
 
-    return { ...result, ipObj };
+    return { ...result };
   } catch (error) {
     throw error;
   }
